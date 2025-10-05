640c20297fc922f002008dc860aa8f06565d05a9
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-10-05 18:06:49 +0700

    236. Challenge: Getting Addresses

--- src/main/java/com/ecommerce/project/controller/AddressController.java
+++ src/main/java/com/ecommerce/project/controller/AddressController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -22,4 +19,7 @@ public class AddressController {
         AddressDTO savedAddressDTO = addressService.createAddress(addressDTO);
         return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
     }
+
+    @GetMapping("/addresses")
+
 }
