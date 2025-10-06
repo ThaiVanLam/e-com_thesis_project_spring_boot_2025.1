755fdf8e3e154ba53e9b9e5d4ee68e3b8b5d9f87
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-10-06 11:44:20 +0700

    241. Setting up the models for Order Management

---
+++ src/main/java/com/ecommerce/project/model/OrderItem.java
@@ -0,0 +1,33 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private Integer quantity;
+    private Double discount;
+    private Double orderedProductPrice;
+}
